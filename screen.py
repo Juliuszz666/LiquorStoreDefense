5f85d7a9b09dcee1b1b27ed64ccf1324b99b1aee
Juliuszz666 <juliuszxxbednarek@gmail.com>
2023-11-28 10:22:24 +0100

    KURWA DZIALA

--- screen.py
+++ screen.py
@@ -19,21 +19,30 @@ def display():
     player_details.fill("green")
     screen.blit(player_details, (0,0))
     
-    costam = pygame.Surface((15, 15))
-    costam.fill("pink")
-    screen.blit(costam, (5,5))
 
-#    eq_item_height = int(player_details.get_height()*0.8)
-#    eq_item_space = int(player_details.get_height()*0.9)
-#    eq_item_base = int(screen.get_height()/100)
-#    
-#    #eq_item = []
-#    
-#    #for i in range(0,9):
-#    equipment_item = pygame.Surface((eq_item_height, eq_item_height))
-#    equipment_item.fill("white")
-#    #    eq_item.append(equipment_item)
-#    player_details.blit(equipment_item, (eq_item_base, eq_item_base))
+    eq_item_height = int(player_details.get_height()*0.8)
+    eq_item_space = int(player_details.get_height()*0.9)
+    eq_item_base = int(screen.get_height()/100)
+    
+    eq_item = []
+    
+    if entities.player.handling_equipment():
+        selected = entities.player.handling_equipment()
+    
+    for i in range(0,9):
+            
+        border = pygame.Surface((eq_item_height, eq_item_height))
+        border.fill("black")
+        if i == selected -1:
+            border.fill("pink")
+        equipment_item = pygame.Surface((eq_item_height-(2*const['ITEM_BORDER']), eq_item_height-(2*const['ITEM_BORDER'])))
+        equipment_item.fill("white")
+        border.blit(equipment_item, (const['ITEM_BORDER'], const['ITEM_BORDER']))
+        eq_item.append(border)
+        
+                   
+    
+        screen.blit(eq_item[i], (eq_item_base + (i*eq_item_space), eq_item_base))
     
     pygame.display.flip()
 
